2396b60f2b33f85c3c9400eb0f2e24d7d3faba6d
sdsd4564 <sdsd4564@gmail.com>
2017-05-15 17:57:26 +0900

    170515 자바스크립트 ㅅ발

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const util = require('util');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Express'});
+});
+
+router.post('/send', function (req, res) {
+    console.log(JSON.stringify(req.body, null, 4));
+    res.send(req.body);
 });
 
 module.exports = router;
