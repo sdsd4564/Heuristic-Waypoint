0acfd6ff09d3d580d80157b6d1ddb35f08e35feb
sdsd4564 <sdsd4564@gmail.com>
2017-05-22 17:50:06 +0900

    170522 API 하루 사용량 제한을 해결하기 위한 스크립트 작성중

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const util = require('util');
 
+// 구글 DIRECTION API 초당 사용량 50개를 피하기 위함.
+let queryLimit = 0;
+
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', {title: 'Express'});
@@ -14,6 +17,17 @@ router.post('/send', function (req, res) {
     dist = JSON.parse(req.body.dist);
 
     res.send(req.body);
+});
+
+router.post('/checkQueryLimit', function (req, res) {
+    queryLimit++;
+    if (queryLimit === 50) {
+        res.send(true);
+        queryLimit = 0;
+    }
+    else {
+        res.send(false);
+    }
 });
 
 module.exports = router;
