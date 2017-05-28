3dbc7ab0d918c12d7f92d9db8a636ae78e41a8b6
sdsd4564 <sdsd4564@gmail.com>
2017-05-28 16:25:39 +0900

    ㅈ같은 구글 디렉션 api

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,69 @@
 const express = require('express');
 const router = express.Router();
-const util = require('util');
+const kruskal = require('node-kruskal');
 
 // 구글 DIRECTION API 초당 사용량 50개를 피하기 위함.
 let queryLimit = 0;
+let checkedTime;
+let coord = [];
+let dist = [];
 
-/* GET home page. */
+
 router.get('/', function (req, res) {
     res.render('index', {title: 'Express'});
 });
 
-router.post('/send', function (req, res) {
-    let obj = [];
-    let dist = [];
-    obj = JSON.parse(req.body.obj);
-    dist = JSON.parse(req.body.dist);
-
-    res.send(req.body);
-});
-
 router.post('/checkQueryLimit', function (req, res) {
-    queryLimit++;
-    if (queryLimit === 50) {
-        res.send(true);
-        queryLimit = 0;
+    if (!checkedTime) {
+        checkedTime = new Date();
     }
-    else {
-        res.send(false);
+    const currentTime = new Date();
+
+
+    if (queryLimit < 50) {
+        console.log("쿼리 센딩 성공");
+        queryLimit++;
+        res.send({
+            "isLimitedOK": true,
+            "remainedTime": 0
+        });
+    } else if (currentTime - checkedTime > 1000) {
+        console.log("쿼리 센딩 실패");
+        res.send({
+            "isLimitedOK": false,
+            "remainedTime": currentTime - checkedTime
+        });
+        queryLimit = 0;
+        checkedTime = null;
     }
 });
+
+router.post('/send', function (req, res) {
+    coord = JSON.parse(req.body.obj);
+    dist = JSON.parse(req.body.dist);
+    console.log(dist);
+    console.log(coord);
+    res.send("성공");
+});
+
+
+router.get('/test', function (req, res) {
+    let distArr = [];
+    for (let i = 0; i < coord.length; i++) {
+        distArr[i] = [];
+        for (let j = 0; j < coord.length; j++)
+            distArr[i][j] = i === j
+                ? 0
+                : i < j
+                    ? dist[(j - 1) * j / 2 + i].distance
+                    : dist[(i - 1) * i / 2 + j].distance;
+    }
+
+    kruskal.kruskalMST(distArr, function (result) {
+        res.send(result);
+    });
+});
+
 
 module.exports = router;
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const us = require('underscore');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/user', function(req, res, next) {
+    res.send("user");
 });
 
 module.exports = router;
